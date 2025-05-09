d06d0f869f6b669ed77419ed295ca7c1e01a44e8
musicTT <lilytoto@naver.com>
2025-05-09 09:59:51 +0000

    이름 입력

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,3 +9,10 @@ st.header("다장조에서 자주 쓰는 코드 진행")
 st.subheader("구성음 3개를 모두 말해주세요!")
 st.image("https://img.freepik.com/premium-vector/piano-key-keyboard-melody-instrument_250246-301.jpg")
 st.snow
+# 한 줄 텍스트 입력
+name = st.text_input("이름을 입력하세요")
+st.write("입력된 이름:", name)
+
+# 여러 줄 텍스트 입력
+feedback = st.text_area("의견을 입력하세요")
+st.write("입력된 의견:", feedback)
