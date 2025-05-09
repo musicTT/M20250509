ff24c7108c7dd352cb96694c0580af86594c4a02
musicTT <lilytoto@naver.com>
2025-05-09 10:07:56 +0000

    이름쓰기까지

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,6 @@ st.title("피아노 코드")
 st.header("다장조에서 자주 쓰는 코드 진행")
 st.subheader("구성음 3개를 모두 말해주세요!")
 st.image("https://img.freepik.com/premium-vector/piano-key-keyboard-melody-instrument_250246-301.jpg")
-st.snow
-# 한 줄 텍스트 입력
 name = st.text_input("이름을 입력하세요")
 st.write("입력된 이름:", name)
 
