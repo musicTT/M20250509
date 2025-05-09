dba7727f978996dada734863aee4702c390bc364
musicTT <lilytoto@naver.com>
2025-05-09 09:20:43 +0000

    대문페이지 자기소개로 바꾸기

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
+st.title("🎈 자기소개")
+st.info("반갑습니다")
+st.write("저는 송내고등학교 음악쌤 입니다")
+# 페이지 구조용 제목 출력
+st.title("피아노 코드")
+st.header("다장조에서 자주 쓰는 코드 진행")
+st.subheader("구성음 3개를 모두 말해주세요!")
